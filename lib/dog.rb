801230709864ed9b83ce9b2cbe69dcae20cf6c08
impartial-flow-3347 <brandon.lau86@gmail.com>
2019-06-05 21:37:05 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+
+class Dog 
+  @@all=[]
+  attr_accessor :name
+  def initialize(name)
+    @name = name 
+    @@all.push(self)
+  end 
+  
+  def self.clear_all
+    @@all.clear
+  end 
+  
+  def self.all 
+    counter = 0 
+    while ( counter < @@all.size)
+      puts @@all[counter].name
+      counter = counter + 1
+    end 
+  end 
+end 
